a04a9bf84391e705f97f1567cae7c7f746f96115
Mima-Dozh <d.k.mozhaev12345@gmail.com>
2022-05-04 00:09:42 +0500

    Add coldown

--- Assets/Scripts/UnitFabrica.cs
+++ Assets/Scripts/UnitFabrica.cs
@@ -10,19 +10,25 @@ public class UnitFabrica : HealthControl
     [SerializeField] private FabricUnitType type;
     [SerializeField] private UnitComponent unit;
     [SerializeField] private ResourcesFabric resources;
+    [SerializeField] private float deltaTime;
     private bool a;
     private Collision2D alsmd;
+    private float lastTime;
     void Start()
     {
         this.SetHealth();
+        lastTime = Time.time;
     }
     
     void OnMouseOver()
     {
+        var time = Time.time;
         if(Input.GetMouseButtonDown(0)
         && unitCost <= resources.resourcesCount
-        && unitCount > 0)
+        && unitCount > 0
+        && time - lastTime > deltaTime)
         {
+            lastTime = time;
             unitCount--;
             resources.resourcesCount -= unitCost;
             Instantiate(unit, transform.position + Vector3.right, Quaternion.identity);
